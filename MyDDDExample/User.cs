9b8cff4cf2f9b723541a2397c5808562b1c7bd96
sou <08thse@gmail.com>
2021-04-11 02:20:00 +0900

    p.55

---
+++ MyDDDExample/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDDDExample
+{
+    class User
+    {
+        private readonly UserId id;
+        private String name;
+
+        public User(UserId id, string name)
+        {
+            if (id == null) throw new ArgumentNullException(nameof(id));
+            this.id = id;
+
+            ChangeUserName(name);
+        }
+
+        private void ChangeUserName(string name)
+        {
+            if (name == null) throw new ArgumentNullException(nameof(name));
+            if (name.Length < 3) throw new ArgumentException("Username must be >3.", nameof(name);
+
+            this.name = name;
+        }
+    }
+}
---
+++ MyDDDExample/UserId.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDDDExample
+{
+    class UserId
+    {
+        private string value;
+
+        public UserId(string value)
+        {
+            if (value == null) throw new ArgumentNullException(nameof(value));
+
+            this.value = value;
+        }
+    }
+}
