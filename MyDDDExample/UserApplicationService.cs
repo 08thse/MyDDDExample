adcdf007eb019e10a1aa52bb4b5fec5ee85db19a
sou <08thse@gmail.com>
2021-04-21 01:13:22 +0900

    p.120

---
+++ MyDDDExample/UserApplicationService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDDDExample
+{
+    public class UserApplicationService
+    {
+        private readonly IUserRepository userRepository;
+        private readonly UserService userService;
+
+        public UserApplicationService(IUserRepository userRepository, UserService userService)
+        {
+            this.userRepository = userRepository;
+            this.userService = userService;
+        }
+
+        public void Register(string name)
+        {
+            var user = new User(new UserName(name));
+
+            if (userService.Exists(user))
+            {
+                // throw new CanNotRegisterUserException(user, "User already exist.");
+            }
+        }
+    }
+}
