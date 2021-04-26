8926aafb84ff7fb6734bc546af775a5fabcc4aa1
sou <08thse@gmail.com>
2021-04-26 23:50:23 +0900

    p.182

--- MyDDDExample/Program.cs
+++ MyDDDExample/Program.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace MyDDDExample
 {
     class Program
     {
+        private static ServiceProvider serviceProvider;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Startup();
+        }
+
+        private static void Startup()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddSingleton<IUserRepository>();
+            // serviceCollection.AddSingleton<IUserRepository, InMemoryUserRepository>();
+            serviceCollection.AddTransient<UserService>();
+            serviceCollection.AddTransient<UserApplicationService>();
+            serviceProvider = serviceCollection.BuildServiceProvider();
         }
     }
 }
